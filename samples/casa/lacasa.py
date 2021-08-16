db1b93fb20fa8e8c3b7eb6cf3e0d7fa84321c367
Javier Candales (Jasón) <javier_candales@yahoo.com.ar>
2021-08-16 12:14:40 -0300

    Add sample 'La Casa'

---
+++ samples/casa/lacasa.py
@@ -0,0 +1,17 @@
+import dfbase
+
+class LaCasa(dfbase.Game):
+
+	def __init__(self) -> None:
+		super().__init__(800, 600)
+
+	def init(self) -> None:
+		self.dictionary.load("templates/dict-debug.xml")
+		self.dictionary.load("templates/dict-es.xml")
+		self.dictionary.load("samples/casa/lacasa.xml")
+
+		self.player = self.dictionary.nouns("player")[0]
+
+if __name__ == "__main__":
+	game = LaCasa()
+	game.run()

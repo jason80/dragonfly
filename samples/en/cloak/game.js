96620e43a24e410dbbc45195aeb0d6bc275313b3
Javier Candales (Jasón) <javier_candales@yahoo.com.ar>
2026-04-27 21:35:02 -0300

    fix: Cloak sample: modify includes

--- samples/en/cloak/game.js
+++ samples/en/cloak/game.js
@@ -1,4 +1,4 @@
-import { Book } from "../../../base/book.js";
+import { Book } from "https://jason80.github.io/dragonfly/base/book.js";
 
 window.onload = () => {
    const book = new Book("game-area", "game.dfml");
